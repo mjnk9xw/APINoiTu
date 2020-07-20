ef40ba9899ad06325352cb6340afddeb134548fa
Nguyễn Văn Minh <minhnv@sonek.vn>
2020-07-20 16:29:29 +0700

    ok

--- main.go
+++ main.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	_ "APINoiTu/routers"
+	"os"
+	"strconv"
 
 	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/plugins/cors"
 )
 
 func main() {
+
+	// beego.BConfig.WebConfig.DirectoryIndex = true
+	// beego.BConfig.WebConfig.StaticDir["/v1/noitu/swagger"] = "swagger"
+
+	// beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+	// 	AllowAllOrigins:  true,
+	// 	AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	// 	AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "content-type", "Content-Type", ""},
+	// 	ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Content-Type"},
+	// 	AllowCredentials: true,
+	// }))
+	// beego.Run()
+
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err == nil {
+		beego.HttpPort = port
+	}
 
 	beego.BConfig.WebConfig.DirectoryIndex = true
 	beego.BConfig.WebConfig.StaticDir["/v1/noitu/swagger"] = "swagger"
