7d91186f38e2b084525720aba50c733ec61b40f4
Nguyễn Văn Minh <minhnv@sonek.vn>
2020-07-20 17:54:41 +0700

    ok

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -10,9 +10,31 @@ func init() {
     beego.GlobalControllerRouter["APINoiTu/controllers:Controller"] = append(beego.GlobalControllerRouter["APINoiTu/controllers:Controller"],
         beego.ControllerComments{
             Method: "Check",
-            Router: `/check`,
+            Router: "/check",
             AllowHTTPMethods: []string{"post"},
             MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["APINoiTu/controllers:RoomController"] = append(beego.GlobalControllerRouter["APINoiTu/controllers:RoomController"],
+        beego.ControllerComments{
+            Method: "GETRoomId",
+            Router: "/:roomId",
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(
+				param.New("roomId", param.IsRequired, param.InPath),
+			),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["APINoiTu/controllers:RoomController"] = append(beego.GlobalControllerRouter["APINoiTu/controllers:RoomController"],
+        beego.ControllerComments{
+            Method: "WS",
+            Router: "/ws/:roomId",
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(
+				param.New("roomId", param.IsRequired, param.InPath),
+			),
             Filters: nil,
             Params: nil})
 
